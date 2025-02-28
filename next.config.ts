bc3f75df468f76dd17f3fa907a051bf2a3a65dda
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-02-28 15:32:22 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  routes:[
-    {
-      src:'/',
-      dest: '/Pages/Home'
-    },
-    {
-      src:'/about',
-      dest: '/Pages/About'
-    },
-    {
-      src:'/services',
-      dest: '/Pages/Services'
-    },
-    {
-      src:'/contact',
-      dest: '/Pages/ContactUs'
-    },
-    {
-      src:'/actuality',
-      dest: '/Pages/Actuality'
-    },
-  ]
+
 };
 
 export default nextConfig;
