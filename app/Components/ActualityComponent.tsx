57009c49d51b959a1e3e373b9356236702f1af12
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-02-28 14:51:35 +0100

    Create ActualityComponent.tsx

---
+++ app/Components/ActualityComponent.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import SectionTitle from './SectionTitle'
+import InfosBtn from './InfosBtn'
+import ActualityBox from './ActualityComponents/ActualityBox'
+
+function ActualityComponent() {
+  return (
+    <div className='z-0 m-0 md:flex block items-center justify-center bg-white w-screen md:h-screen h-auto mt-20 md:mt-0'>
+      <div className='max-w-screen-2xl w-full h-auto py-8 px-4'>
+        <div className='h-auto lg:w-1/2 flex-col gap-4'>
+          <SectionTitle title={"Nos Actualités"} />
+          <h1 className='font-medium md:text-[18px] text-[15px] text-[#D3D63F] mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos autem repellendus impedit, aliquid temporibus! Voluptatibus beatae aut ab earum nesciunt, mollitia architecto similique, nemo dolorum eum vero fuga cum?</h1>
+        </div>
+        <div className='flex flex-wrap justify-between items-center w-full h-auto md:p-4 gap-2'>
+          <ActualityBox
+          link={"/pages/ActualityItems"}
+          image={"/act-img1.png"}
+          badge={"visite"}
+          title={"Visite de travail d'une délégation gambienne au port autonome"}
+          date={"15 Decembre 2024 à 20h05"}
+          />
+          <ActualityBox
+          link={"/pages/ActualityItems"}
+          image={"/act-img2.png"}
+          badge={"Annulation"}
+          title={"Annulation des frais de magasinage"}
+          date={"21 Novembre 2024 à 20h 01"}
+          />
+          <ActualityBox
+          link={"/pages/ActualityItems"}
+          image={"/act-img3.png"}
+          badge={"Séminaire"}
+          title={"Séminaire annuel du reseau de femmes professionelles."}
+          date={"12 Novembre 2024 à 19h 34"}
+          />
+        </div>
+        <div className='w-full flex items-center justify-center mt-10'>
+          <InfosBtn title={"Toutes les Actualités"}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ActualityComponent
