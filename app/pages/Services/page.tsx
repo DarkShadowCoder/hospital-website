dd8e8333c59dee339a8fa6ca5f9f86013e9a2331
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-02-28 14:52:26 +0100

    Create page.tsx

---
+++ app/pages/Services/page.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import MinHeader from '../../Components/HeaderComponents/MinHeader'
+import SectionTitle from '../../Components/SectionTitle'
+import ServicesBox from '../../Components/ServicesComponents/ServicesBox'
+import Footer from '../../Components/Footer'
+
+function Services() {
+  return (
+    <div>
+      <MinHeader
+      title={"Nos Services"}
+      description={"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression."}
+      />
+      <div className='z-0 flex flex-col w-full items-center justify-center px-4 gap-8 pb-8 md:pb-10 bg-white'>
+        <div className='max-w-screen-2xl flex items-center z-0'>
+          <SectionTitle title={"Nos Services"} />
+        </div>
+        <div className='grid md:grid-cols-3 xl:grid-cols-4 grid-rows-3 gap-6 '>
+          <ServicesBox 
+          icon={"/placeholder1.png"}
+          title="Consultation médicale"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder2.png"}
+          title="Médecine Générale"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder3.png"}
+          title="Urgences"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder4.png"}
+          title="Chirurgie"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder1.png"}
+          title="Consultation médicale"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder2.png"}
+          title="Médecine Générale"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder3.png"}
+          title="Urgences"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+          <ServicesBox 
+          icon={"/placeholder4.png"}
+          title="Chirurgie"
+          description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ipsam vel? Dolor quae magni necessitatibus unde deleniti sequi rem voluptas impedit laudantium vero vitae temporibus, doloribus ut mollitia, delectus error!"}
+          pair={true}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default Services
