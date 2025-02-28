7740ac42ee23f3713da88c515f9d01c175022b43
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-02-28 14:51:42 +0100

    Create InfosBtn.tsx

---
+++ app/Components/InfosBtn.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function InfosBtn(props) {
+  return (
+    <a
+      href={props.link} 
+      className='min-w-[150px] w-auto h-[50px] bg-[#047424] rounded-[25px] hover:text-[#D3D63F] hover:transition duration-300 ease-in-out md:text-[20px] text-[18px] font-bold px-8 flex items-center justify-center cursor-pointer'
+      >
+      <h1>{props.title}</h1>
+    </a>
+  )
+}
+
+export default InfosBtn
