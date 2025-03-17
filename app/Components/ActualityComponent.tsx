2c6d7449bb038244dbf3fff8cd66507a2b552013
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-03-17 15:13:43 +0100

    Update ActualityComponent.tsx

--- app/Components/ActualityComponent.tsx
+++ app/Components/ActualityComponent.tsx
@@ -6,7 +6,7 @@ import ActualityBox from './ActualityComponents/ActualityBox'
 function ActualityComponent() {
   return (
     <div className='z-0 m-0 lg:flex block items-center justify-center bg-white w-screen lg:h-screen h-auto mt-20 lg:mt-0'>
-      <div className='max-w-screen-2xl w-full h-auto py-8 px-4'>
+      <div className='max-w-screen-xl w-full h-auto py-8 px-4'>
         <div className='h-auto lg:w-1/2 flex-col gap-4'>
           <SectionTitle title={"Nos Actualités"} />
           <h1 className='font-medium lg:text-[18px] text-[15px] text-[#D3D63F] mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos autem repellendus impedit, aliquid temporibus! Voluptatibus beatae aut ab earum nesciunt, mollitia architecto similique, nemo dolorum eum vero fuga cum?</h1>
