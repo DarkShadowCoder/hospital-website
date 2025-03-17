b483c17ca5c2f7fd7159a7202dad3a32eeff6369
DarkShadowCoder <mbohoumounpouyvanlandry@gmail.com>
2025-03-17 15:15:11 +0100

    Update page.tsx

--- app/pages/ActualityItems/page.tsx
+++ app/pages/ActualityItems/page.tsx
@@ -1,97 +1,106 @@
+"use client";
 import React from "react";
 import Navbar from "../../Components/HeaderComponents/Navbar";
 import Footer from "../../Components/Footer";
 import Image from "next/image";
+import AnimatedDiv from "@/app/Components/Animation/AnimatedDiv";
 
 function ActualityItems() {
   return (
     <div>
-      <Navbar />
-      <div className="w-screen pb-14 h-auto flex items-center justify-center pt-[150px]">
-        <div className="w-full h-auto max-w-screen-2xl flex flex-col p-4 text-black text-justify">
-          <div className="flex flex-col-reverse md:flex-row flex-wrap h-auto w-full">
-            <p className="flex flex-col gap-4 px-4 md:w-1/2 w-full h-full">
-              <h1 className="md:text-[26px] text-[24px] font-bold text-center">
-                Hopital Regional Annexe de Foumban : Le nouveau directeur prend
-                service
-              </h1>
-              <p className="text-[17px] font-normal">
-                lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-                null nulla null Lorem ips lorem ipsum dolor sit amet,
-                consectetur adipiscing elit sapien null nulla null Lorem ips
-                lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-                null nulla null Lorem ips lorem ipsum dolor sit amet,
-                consectetur adipiscing elit sapien null nulla null Lorem ips
-                lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-                null nulla null Lorem ips
+      <AnimatedDiv>
+        <Navbar />    
+      </AnimatedDiv>
+      <AnimatedDiv>
+            <div className="w-screen pb-14 h-auto flex items-center justify-center pt-[150px]">
+          <div className="w-full h-auto max-w-screen-xl flex flex-col p-4 text-black text-justify">
+            <div className="flex flex-col-reverse md:flex-row flex-wrap h-auto w-full">
+              <p className="flex flex-col gap-4 px-4 md:w-1/2 w-full h-full">
+                <h1 className="md:text-[26px] text-[24px] font-bold text-center">
+                  Hopital Regional Annexe de Foumban : Le nouveau directeur prend
+                  service
+                </h1>
+                <p className="text-[17px] font-normal">
+                  lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+                  null nulla null Lorem ips lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit sapien null nulla null Lorem ips
+                  lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+                  null nulla null Lorem ips lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit sapien null nulla null Lorem ips
+                  lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+                  null nulla null Lorem ips
+                </p>
               </p>
-            </p>
-            <Image
-              src={"/image 12.png"}
-              alt="Image"
-              width={700}
-              height={300}
-              className="md:h-[400px] md:w-auto w-full "
-            />
+              <Image
+                src={"/image 12.png"}
+                alt="Image"
+                width={700}
+                height={300}
+                className="md:h-[400px] md:w-auto w-full "
+              />
+            </div>
+            <div className="h-full flex flex-wrap text-[17px] font-normal p-4">
+              lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
+              nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ip lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips
+              <br />
+              <br />
+              lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
+              nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips
+              <br />
+              <br />
+              lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
+              nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips
+              <br />
+              <br />
+              lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
+              nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
+              adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
+              sit amet, consectetur adipiscing elit sapien null nulla null Lorem
+              ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
+              null nulla null Lorem ips
+            </div>
           </div>
-          <div className="h-full flex flex-wrap text-[17px] font-normal p-4">
-            lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
-            nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ip lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips
-            <br />
-            <br />
-            lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
-            nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips
-            <br />
-            <br />
-            lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
-            nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips
-            <br />
-            <br />
-            lorem ipsum dolor sit amet, consectetur adipiscing elit sapien null
-            nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips lorem ipsum dolor sit amet, consectetur
-            adipiscing elit sapien null nulla null Lorem ips lorem ipsum dolor
-            sit amet, consectetur adipiscing elit sapien null nulla null Lorem
-            ips lorem ipsum dolor sit amet, consectetur adipiscing elit sapien
-            null nulla null Lorem ips
-          </div>
-        </div>
-      </div>
-      <Footer />
+        </div>    
+      </AnimatedDiv>
+      <AnimatedDiv>
+          <Footer />    
+      </AnimatedDiv>
+      
     </div>
   );
 }
